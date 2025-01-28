a78b59936274fee9f029396f63554dde5eb5f6fa
SerhiyStp <sstepanchuk@gmail.com>
2025-01-28 14:54:25 +0000

    Saves simulation output to a file

--- main.f90
+++ main.f90
@@ -295,19 +295,26 @@ program Laffer
 
                 call Statistics_to_file(output_unit)
 
-                !open(1, file='singledist.txt')
-                !
-                !write (1, *) fpartner,mpartner
-                !
-                !close(1)
-                !
-                !open(1, file='abilityprob.txt')
-                !
-                !write (1, *) ability_prob
-                !
-                !close(1)
-                !
-                !STOP
+                open(1, file='singledist.txt')
+                write (1, *) fpartner,mpartner
+                close(1)
+
+                open(1, file='abilityprob.txt')
+                write (1, *) ability_prob
+                close(1)
+
+                !Variables are age, gender, ID number, weight, marital status, asset holdings, household labor income, Household_Labor_Income_Tax_Paid,  Household_consumption_Tax_Paid, ability, hours, earnings
+
+                open(1, file='Simulation_output.txt')
+                do it2=1,T
+                    do it3=1,nsim2
+                        do it4=1,nsim
+                            write (1,'(F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4)') it2*1d0, 1d0, nsim*(it3-1)*1d0+it4*1d0, 1d0, Sim1m(it3,it4,it2,10), Sim1m(it3,it4,it2,1), Sim1m(it3,it4,it2,6), Sim1m(it3,it4,it2,7), Sim1m(it3,it4,it2,8), exp1m(it3,it4,it2,2)*1d0, Sim1m(it3,it4,it2,4), Sim1m(it3,it4,it2,5)  
+                            write (1,'(F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4,F12.4)') it2*1d0, 2d0, nsim*(it3-1)*1d0+it4*1d0, 1d0, Sim1f(it3,it4,it2,10), Sim1f(it3,it4,it2,1), Sim1f(it3,it4,it2,6), Sim1f(it3,it4,it2,7), Sim1f(it3,it4,it2,8), exp1f(it3,it4,it2,2)*1d0, Sim1f(it3,it4,it2,4), Sim1f(it3,it4,it2,5)
+                        end do
+                    end do
+                end do
+                close(1)
 
                 Print *,'epsilon is',epsilon
                 Print *,'epsilon2 is',epsilon2
