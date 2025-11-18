af0b605a888455eb43c224a2786891a6b25a7e5a
SerhiyStp <sstepanchuk@gmail.com>
2025-11-18 14:43:12 +0000

    Fixes the problems with argument dimensionality in labor1 and labors subroutines

--- labor1.f90
+++ labor1.f90
@@ -4,18 +4,18 @@ SUBROUTINE labor1 (X, F, N)
     USE glob0
     USE Utilities
     INTEGER N
-    real(8) X(N), F(N)
+    real(8) X, F
     real(8) :: dum2,Y1
     
     if(ind2==1) then
         !Y1 is consumption to be used in the FOC
         Y1=dum
-        X(1)=max(X(1),0.0001d0)
-        F(1)=wagem*(1d0-(tax_labor(wagem*X(1))+DtSS_employee(wagem*X(1))))-(1d0/AE)*(wagem**2)*X(1)*theta(1)*theta(2)*((wagem*X(1))*(1d0/AE))**(-1d0-theta(2))-chim*(X(1)**etam)*Y1*(1d0+tc)
+        X=max(X,0.0001d0)
+        F=wagem*(1d0-(tax_labor(wagem*X)+DtSS_employee(wagem*X)))-(1d0/AE)*(wagem**2)*X*theta(1)*theta(2)*((wagem*X)*(1d0/AE))**(-1d0-theta(2))-chim*(X**etam)*Y1*(1d0+tc)
     else
         Y1=dum
-        X(1)=max(X(1),0.0001d0)
-        F(1)=wagem*(1d0-(tax_labor(wagem*X(1))+DtSS_employee(wagem*X(1))))-(1d0/AE)*(wagem**2)*X(1)*theta(1)*theta(2)*((wagem*X(1))*(1d0/AE))**(-1d0-theta(2))-chif*(X(1)**etaf)*Y1*(1d0+tc)
+        X=max(X,0.0001d0)
+        F=wagem*(1d0-(tax_labor(wagem*X)+DtSS_employee(wagem*X)))-(1d0/AE)*(wagem**2)*X*theta(1)*theta(2)*((wagem*X)*(1d0/AE))**(-1d0-theta(2))-chif*(X**etaf)*Y1*(1d0+tc)
     end if
         
 end subroutine labor1
@@ -24,6 +24,6 @@ subroutine labor1_hybrd(n, x, fvec, iflag)
     integer :: n, iflag
     real(8) :: x(n), fvec(n)
 
-    call labor1(x, fvec, n)
+    call labor1(x(1), fvec(1), n)
     
 end subroutine labor1_hybrd
--- labors.f90
+++ labors.f90
@@ -4,18 +4,18 @@ SUBROUTINE labors (X, F, N)
     USE glob0
     USE Utilities
     INTEGER N
-    real(8) X(N), F(N)
+    real(8) X, F
     real(8) :: dum2,Y1
 
     !Y1 is consumption to be used in the FOC
     if(ind2==1) then
         Y1=dum
-        X(1)=max(X(1),0.0001d0)
-        F(1)=wagem*(1d0-( tax_labors(wagem*X(1)) + DtSS_employee(wagem*X(1)) ))-(1d0/AE)*(wagem**2d0)*X(1)*thetas(1)*thetas(2)*((wagem*X(1))*(1d0/AE))**(-1d0-thetas(2))-chims*(X(1)**etam)*Y1*(1d0+tc)
+        X=max(X,0.0001d0)
+        F=wagem*(1d0-( tax_labors(wagem*X) + DtSS_employee(wagem*X) ))-(1d0/AE)*(wagem**2d0)*X*thetas(1)*thetas(2)*((wagem*X)*(1d0/AE))**(-1d0-thetas(2))-chims*(X**etam)*Y1*(1d0+tc)
     else
         Y1=dum
-        X(1)=max(X(1),0.0001d0)
-        F(1)=wagem*(1d0-( tax_labors(wagem*X(1)) + DtSS_employee(wagem*X(1)) ))-(1d0/AE)*(wagem**2)*X(1)*thetas(1)*thetas(2)*((wagem*X(1))*(1d0/AE))**(-1d0-thetas(2))-chifs*(X(1)**etaf)*Y1*(1d0+tc)
+        X=max(X,0.0001d0)
+        F=wagem*(1d0-( tax_labors(wagem*X) + DtSS_employee(wagem*X) ))-(1d0/AE)*(wagem**2)*X*thetas(1)*thetas(2)*((wagem*X)*(1d0/AE))**(-1d0-thetas(2))-chifs*(X**etaf)*Y1*(1d0+tc)
     end if
     
 end subroutine labors
@@ -25,6 +25,6 @@ subroutine labors_hybrd(n, x, fvec, iflag)
     integer :: n, iflag
     real(8) :: x(n), fvec(n)
 
-    call labors(x, fvec, n)
+    call labors(x(1), fvec(1), n)
     
 end subroutine labors_hybrd
